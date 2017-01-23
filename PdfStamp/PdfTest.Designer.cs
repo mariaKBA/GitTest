99e7f73edea2640970ef323dd71fbdb7d7bf8529
marott <marott@ADMDSEM031601>
2017-01-23 15:19:41 +0100

    Pdf-stamp

---
+++ PdfStamp/PdfTest.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PdfStamp
+{
+    partial class PdfTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bShowGrade = new System.Windows.Forms.Button();
+            this.bShowStamp = new System.Windows.Forms.Button();
+            this.bCreateAndShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bShowGrade
+            // 
+            this.bShowGrade.Location = new System.Drawing.Point(26, 29);
+            this.bShowGrade.Name = "bShowGrade";
+            this.bShowGrade.Size = new System.Drawing.Size(142, 29);
+            this.bShowGrade.TabIndex = 0;
+            this.bShowGrade.Text = "Titta på betyg";
+            this.bShowGrade.UseVisualStyleBackColor = true;
+            this.bShowGrade.Click += new System.EventHandler(this.bShowGrade_Click);
+            // 
+            // bShowStamp
+            // 
+            this.bShowStamp.Location = new System.Drawing.Point(26, 76);
+            this.bShowStamp.Name = "bShowStamp";
+            this.bShowStamp.Size = new System.Drawing.Size(142, 30);
+            this.bShowStamp.TabIndex = 1;
+            this.bShowStamp.Text = "Titta på stämpel";
+            this.bShowStamp.UseVisualStyleBackColor = true;
+            this.bShowStamp.Click += new System.EventHandler(this.bShowStamp_Click);
+            // 
+            // bCreateAndShow
+            // 
+            this.bCreateAndShow.Location = new System.Drawing.Point(26, 131);
+            this.bCreateAndShow.Name = "bCreateAndShow";
+            this.bCreateAndShow.Size = new System.Drawing.Size(193, 30);
+            this.bCreateAndShow.TabIndex = 2;
+            this.bCreateAndShow.Text = "Skapa ny pdf med stämpel";
+            this.bCreateAndShow.UseVisualStyleBackColor = true;
+            this.bCreateAndShow.Click += new System.EventHandler(this.bCreateAndShow_Click);
+            // 
+            // PdfTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.bCreateAndShow);
+            this.Controls.Add(this.bShowStamp);
+            this.Controls.Add(this.bShowGrade);
+            this.Name = "PdfTest";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bShowGrade;
+        private System.Windows.Forms.Button bShowStamp;
+        private System.Windows.Forms.Button bCreateAndShow;
+    }
+}
+
---
+++ PdfStamp/PdfTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace PdfStamp
+{
+    public partial class PdfTest : Form
+    {
+        public PdfTest()
+        {
+            InitializeComponent();
+        }
+
+        private void bShowGrade_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\src\Visual Studio 2015\Projects\GitTest\PdfStamp\files\betygA.pdf";
+            System.Diagnostics.Process.Start(filePath);
+
+            /*Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            process.StartInfo = startInfo;
+            startInfo.FileName = @"C:\src\Visual Studio 2015\Projects\GitTest\PdfStamp\files\betyg.pdf";
+            process.Start();*/
+        }
+
+        private void bShowStamp_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\src\Visual Studio 2015\Projects\GitTest\PdfStamp\files\stampel.pdf";
+            System.Diagnostics.Process.Start(filePath);
+        }
+
+        private void bCreateAndShow_Click(object sender, EventArgs e)
+        {
+            String gradePath = @"C:\src\Visual Studio 2015\Projects\GitTest\PdfStamp\files\betygA.pdf";
+            String stampPath = @"C:\src\Visual Studio 2015\Projects\GitTest\PdfStamp\files\stampel.pdf";
+            //TODO: Valbar eller ta bort
+            String stampedGradePath = @"C:\temp\stampedGrade.pdf";
+
+            StampPdf(gradePath, stampPath, stampedGradePath);
+
+            //Visa
+            System.Diagnostics.Process.Start(stampedGradePath);
+        }
+
+        private void StampPdf(String iPdfPath, String iStampPath, String iStampedPdfPath)
+        {
+            PdfReader pdfReader = new PdfReader(iPdfPath);
+            PdfReader stampReader = new PdfReader(iStampPath);
+
+            FileStream stampedFile = new FileStream(iStampedPdfPath, FileMode.Create);
+            PdfStamper PDFStamper = new PdfStamper(pdfReader, stampedFile);
+
+            // Stamp every page
+            PdfImportedPage page = PDFStamper.GetImportedPage(stampReader, 1);
+            int n = pdfReader.NumberOfPages;
+            PdfContentByte stampInfo;
+            for (int i = 1; i <= n; i++)
+            {
+                stampInfo = PDFStamper.GetOverContent(i);
+                stampInfo.AddTemplate(page, 100, 100);
+            }
+            // Close the stamper
+            PDFStamper.Close();
+
+        }
+    }
+}
